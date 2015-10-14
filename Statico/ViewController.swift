0a422083231a36fa6fe35a6a0047ad8014f9d23d
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-10-14 21:30:26 +1100

    Xcode template app

---
+++ Statico/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Statico
+//
+//  Created by Giovanni on 14/10/2015.
+//  Copyright © 2015 mokacoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
